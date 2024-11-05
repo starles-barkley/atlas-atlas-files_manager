4fe89697224e46220842783ac9647dc00cc065b5
kvang2 <kevinvaj88@hotmail.com>
2024-11-05 15:13:54 -0600

    updating the redis.js file with the get,set del function

--- utils/redis.js
+++ utils/redis.js
@@ -13,11 +13,50 @@ class RedisClient {
             console.error('Redis connection error:', err);
         });
     }
-        isAlive() {
-            return this.client.connected;
-        }
 
-        async get(key) {
-            
-        }
+    isAlive() {
+        return this.client.connected;
     }
+
+    async get(key) {
+        return new Promise((resolve, reject) => {
+            this.client.get(key, (err, value) => {
+                if (err) {
+                    console.error('Error', err);
+                    reject(err);
+                } else {
+                    resolve(value);
+                }
+            });
+        });
+    }
+
+    async set(key, value, duration) {
+        return new Promise((resolve, reject) => {
+            this.client.set(key, value, 'EX', duration, (err, reply) => {
+                if (err) {
+                    console.error('Error', err);
+                    reject(err);
+                } else {
+                    resolve(reply);
+                }
+            });
+        });
+    }
+
+    async del(key) {
+        return new Promise((resolve, reject) => {
+            this.client.del(key, (err, reply) => {
+                if (err) {
+                    console.error('Error', err);
+                    reject(err);
+                } else{
+                    resolve(reply);
+                }
+            });
+        });
+    }
+}
+
+const redisClient = new RedisClient();
+module.exports = redisClient;
