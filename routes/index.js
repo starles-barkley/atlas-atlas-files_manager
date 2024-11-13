6c53b63c888e5b57aaa5eb45e8f66a18e159cdb4
kvang2 <kevinvaj88@hotmail.com>
2024-11-13 09:49:35 -0600

    update task 7 on FilesController.js and index.js on two new endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController.js';
 import UsersController from '../controllers/UsersController.js';
-import FilesController from '../controllers/UsersController.js';
+import FilesController from '../controllers/FilesController.js';
 
 const router = Router();
 
@@ -16,6 +16,10 @@ router.post('/users', UsersController.postNew);
 
 // Add new endpoint
 router.post('/files', FilesController.postUpload);
+
+// add PUT endpoints
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
 
 // Export the router
 export default router;
